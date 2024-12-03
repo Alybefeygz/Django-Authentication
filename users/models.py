3ead4af6adf4f363d9c119410918966ac85dc917
Yağız Efe ALAYBAY <ygzefealyb@gmail.com>
2024-12-03 11:52:18 +0300

    User models.py Commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    #Ek alanlar oluştur.
+    bio = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=30, null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
