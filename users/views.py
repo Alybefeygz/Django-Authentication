6d979b3f1d715746ab8515571589b9fa6543a74b
Yağız Efe ALAYBAY <ygzefealyb@gmail.com>
2024-12-13 12:50:54 +0300

    Views.py First Commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomerUserCreationForms, CustomAuthenticationForm, AuthenticationForm
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # 'home.html' şablonunu render ederek kullanıcıya döndürüyoruz ve context'e request.user'ı ekliyoruz
+    return render(request, 'home.html', {'user': request.user})
